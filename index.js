445cddece8bd23f868d6cdd047ad0d93d895b6fd
adityaharsh2001 <geekyadi118@gmail.com>
2021-08-15 12:56:06 +0000

    server setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { post } = require('got');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('fuck');
+});
+
+app.post("/run", (req, res) => {
+    
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
