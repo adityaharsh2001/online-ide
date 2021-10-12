2f7c3e0e1fa452bbae5cae8ad6a8e48d14f66f3a
Aditya Harsh <adityaharsh@geekyadi.local>
2021-10-12 12:46:49 +0530

    dockerize dev server

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:latest
+WORKDIR /opt/app/src
+
+
+RUN  apt-get update && apt-get install -y redis-server
+
+COPY package.json ./
+COPY package-lock.json ./
+
+RUN npm install --no-optional && npm cache clean --force
+RUN npm i -g nodemon
+
+COPY . . 
+
+EXPOSE 6379
+EXPOSE 5000
+
+CMD ["sh", "-c", "redis-server > /dev/null 2>&1 & npm run dev"]
--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./api/routes/routes");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 require("dotenv").config();
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri,
-  err => {
-      if(err) throw err;
-      console.log('connected to MongoDB')
-  });
+mongoose.connect(uri, (err) => {
+  if (err) throw err;
+  console.log("connected to MongoDB");
+});
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", routes);
-
+app.use("/", routes);  
 app.listen(5000, () => {
   console.log(`Listening on port 5000!`);
 });
+ 
