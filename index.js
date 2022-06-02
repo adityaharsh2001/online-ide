a513f6c054ddc4c72c0e9e58728ecc90aaf6613b
adityaharsh2001 <geekyadi118@gmail.com>
2022-06-02 17:13:33 +0530

    fixed proxy

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use("/api", routes);
 
