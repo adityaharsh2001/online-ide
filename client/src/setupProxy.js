63a968ea063417d72711262704f61ec3a70edb82
adityaharsh2001 <geekyadi118@gmail.com>
2022-06-03 00:01:14 +0530

    fixed docker reverse proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const  createProxyMiddleware  = require('http-proxy-middleware');
 console.log(process.env.CLIENT_API_BASE_URL);
 module.exports = function(app) {
   app.use(
--- index.js
+++ index.js
@@ -22,7 +22,12 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, './client/build')));
+//app.use(express.static(path.join(__dirname, './client/build')));
+const root  = path.join(__dirname, 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+});
 
 app.use("/api", routes);
 
