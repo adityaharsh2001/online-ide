5c30818caee1e6c0dd049b7f8267553026c8718c
Aditya Harsh <geekyadi118@gmail.com>
2022-06-19 23:48:57 +0530

    fixed child process stdout

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.NODE_ENV === "production"? process.env.REACT_APP_API_BASE_URL: process.env.CLIENT_API_BASE_URL,
+      target: REACT_APP_API_BASE_URL,
       changeOrigin: true,
     })
   );
